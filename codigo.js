4827df2ad6795876236018dcd3ba4f8f9df592e6
lad <lad@facultades.ort.edu.uy>
2025-09-02 19:45:34 -0300

    nombre

--- codigo.js
+++ codigo.js
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
       const groupBody = group.querySelector('.faq-group-body');
       const icon = groupHeader.querySelector('i');
   
-      // Toggle icon
+      
       icon.classList.toggle('fa-plus');
       icon.classList.toggle('fa-minus');
   
-      // Toggle visibility of body
+     
       groupBody.classList.toggle('open');
   
-      // Close other open FAQ bodies
+      
       const otherGroups = faqContainer.querySelectorAll('.faq-group');
   
       otherGroups.forEach((otherGroup) => {
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
-  // Mobile Menu
+  
   document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger-button');
     const mobileMenu = document.querySelector('.mobile-menu');
