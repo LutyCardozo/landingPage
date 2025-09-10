373debe424805d2fe7fed8b1d319a3a2ffdd7b64
luty <lucianacardozomello@gmail.com>
2025-09-09 21:39:04 -0300

    prueba

--- codigo.js
+++ codigo.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (faqContainer) {
     faqContainer.addEventListener('click', (e) => {
       const groupHeader = e.target.closest('.faq-group-header');
+      if (!groupHeader) return;
 
       if (!groupHeader) return;
 
